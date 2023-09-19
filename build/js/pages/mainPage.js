fd6ce80919d80dfa3f8a6fc199d09963ec160373
mishadawg <70436514+mishadawg@users.noreply.github.com>
2023-09-19 12:33:32 +0300

    Delete build directory

--- build/js/pages/mainPage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-(function ($) {
-  $(document).ready(function () {
-    console.log('main page file');
-  });
-})(jQuery);
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGFnZXMvbWFpblBhZ2UuanMiLCJuYW1lcyI6WyIkIiwiZG9jdW1lbnQiLCJyZWFkeSIsImNvbnNvbGUiLCJsb2ciLCJqUXVlcnkiXSwic291cmNlcyI6WyJwYWdlcy9tYWluUGFnZS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24gKCQpIHtcbiAgICAkKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbigpe1xuICAgICAgICBjb25zb2xlLmxvZygnbWFpbiBwYWdlIGZpbGUnKTtcbiAgICB9KTtcbn0pKGpRdWVyeSk7Il0sIm1hcHBpbmdzIjoiOztBQUFBLENBQUMsVUFBVUEsQ0FBQyxFQUFFO0VBQ1ZBLENBQUMsQ0FBQ0MsUUFBUSxDQUFDLENBQUNDLEtBQUssQ0FBQyxZQUFVO0lBQ3hCQyxPQUFPLENBQUNDLEdBQUcsQ0FBQyxnQkFBZ0IsQ0FBQztFQUNqQyxDQUFDLENBQUM7QUFDTixDQUFDLEVBQUVDLE1BQU0sQ0FBQyJ9
